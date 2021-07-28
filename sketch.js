a2d501227b35741029ca2bcdfc81ad2ac08db673
yangchristina <69733531+yangchristina@users.noreply.github.com>
2021-07-28 16:25:00 -0700

    finished video

--- sketch.js
+++ sketch.js
@@ -1,13 +1,22 @@
-function setup() {
-    let c = createCanvas(windowWidth, windowWidth);
-    c.position(0,0);
-    clear()
-    console.log("set up")
-}
+console.log('sketch blah');
 
-function draw() {
-    console.log("looping")
-    stroke(0)
-    strokeWeight(4)
-    line(mouseX, mouseY, pmouseX, pmouseY)
-}
+var s = function(sketch) {
+  sketch.setup = function() {
+    document.body.style['userSelect'] = 'none';
+    let h = document.body.clientHeight;
+    let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+    c.position(0, 0);
+    c.style('pointer-events', 'none');
+    sketch.clear();
+  };
+
+  sketch.draw = function() {
+    sketch.stroke(0);
+    sketch.strokeWeight(4);
+    if (sketch.mouseIsPressed) {
+      sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+    }
+  };
+};
+
+var myp5 = new p5(s);
