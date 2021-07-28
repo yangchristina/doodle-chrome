fcb48ccb0c9b2fb5982e237dadbac31cf06ef5b0
yangchristina <69733531+yangchristina@users.noreply.github.com>
2021-07-28 16:14:01 -0700

    mouse draws line, removed bg

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 function setup() {
-    let c = createCanvas(400, 400)
-    c.position(0,0)
+    let c = createCanvas(windowWidth, windowWidth);
+    c.position(0,0);
+    clear()
     console.log("set up")
 }
 
 function draw() {
     console.log("looping")
+    stroke(0)
+    strokeWeight(4)
+    line(mouseX, mouseY, pmouseX, pmouseY)
 }
