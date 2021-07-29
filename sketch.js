f9a20aa7be7947a1e6a247cd4ef44916ffc091b2
yangchristina <69733531+yangchristina@users.noreply.github.com>
2021-07-28 17:19:11 -0700

    change brush color when 'c' is pressed, change brush size with up and down arrows, clear canvas when 'z' is pressed

--- sketch.js
+++ sketch.js
@@ -1,22 +1,67 @@
-console.log('sketch blah');
+//issues
+/*
+    can't draw over certain things
+    can't always hilight text
+    must refresh page first to work
+*/
 
 var s = function(sketch) {
-  sketch.setup = function() {
-    document.body.style['userSelect'] = 'none';
-    let h = document.body.clientHeight;
-    let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-    c.position(0, 0);
-    c.style('pointer-events', 'none');
-    sketch.clear();
-  };
+    let size;
+    let color;
 
-  sketch.draw = function() {
-    sketch.stroke(0);
-    sketch.strokeWeight(4);
-    if (sketch.mouseIsPressed) {
-      sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+    sketch.setup = function() {
+        document.body.style['userSelect'] = 'none';
+        let h = document.body.clientHeight;
+        let c = sketch.createCanvas(document.body.clientWidth, document.body.clientWidth);
+        c.position(0, 0);
+        c.style('pointer-events', 'none');
+
+        //variables
+        size = 4
+        color = 255
+
+        sketch.clear();
+    };
+
+    sketch.draw = function() {
+        sketch.Color();
+        sketch.BrushSize();
+        sketch.Paint();
+
+        if (sketch.keyIsDown(90)) {
+            sketch.clear();
+        }
+    };
+
+    sketch.Paint = function() {
+        if (sketch.keyIsDown(sketch.SHIFT)) {
+            sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+            }
     }
-  };
+    sketch.Color = function() {
+        sketch.colorMode(sketch.HSB)
+        
+        if (sketch.keyIsDown(67) && color < 360) {
+            color += 50
+            if(color > 360)
+                color= color-360
+        }
+        sketch.stroke(color, 204, 100);
+    }
+    sketch.BrushSize = function() {
+        if (sketch.keyIsDown(sketch.UP_ARROW)&& size < 400) {
+            size++
+        }
+        else if (sketch.keyIsDown(sketch.DOWN_ARROW) && size > 0) {
+            size--
+        }
+        sketch.strokeWeight(size);
+        console.log(size)
+    }
+    sketch.Clear = function() {
+        sketch.clear();
+    }
+
 };
 
 var myp5 = new p5(s);
